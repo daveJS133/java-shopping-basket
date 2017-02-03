0d14aeccc2db7b4085821fa1c020bad48dbbc007
Dave Scott <davejs133@gmail.com>
2017-02-03 19:46:45 +0000

    customer and basket created

--- Basket.java
+++ Basket.java
@@ -1,4 +1,30 @@
-import org.java.*;
+import org.java.util.*;
+
+
+
+public class Basket{
+  private HashMap<String, Item> basketHash
+  private String customerName;
+  private int customerId;
+  private int basketId;
+
+  public Basket(customerId, basketId){
+    this.basketHash = new HashMap<String, Item>();
+    this.name = Customer.getName(customerId);
+  }
+
+  private void addItem(product){
+    Item item = new Item(Item.generateId, product);
+    basketHash.put(item.getId(), item);
+  }
+
+  private void removeItem(id){
+    basketHash.remove(id);
+  }
+
+
+
+}
 
 
 
---
+++ Customer.java
@@ -0,0 +1,16 @@
+import org.java.util.*;
+
+
+
+public class Customer{
+  private Basket basket;
+  private String customerName;
+  private int customerId;
+  private int basketId;
+
+  public Customer(name){
+    Basket this.basket = new Basket(this.createCustomerId, this.createBasketId());
+    this.customerName = name; 
+  }
+
+}
